61e9505fa797efc72592d07726ebfe246eb9a238
seha <hanseha17@gmail.com>
2026-07-20 19:54:57 +0900

    Spring Security설정2

--- Backend/src/main/java/com/example/nailyproject/auth/SecurityConfig.java
+++ Backend/src/main/java/com/example/nailyproject/auth/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
